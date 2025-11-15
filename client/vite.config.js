3214ac2f52258e8874c25f4c9532df012a79ddfb
MdRubel <emailtorubel@gmail.com>
2025-11-14 21:53:50 -0500

    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname), // <--- ensures Vite looks in client/ for index.html
   plugins: [react(), tailwindcss()],
 })
